b2428f82d2ee19cbd624b22ea0ddf26f2f42076d
djd4rkn355 <k.duezgoeren@gmail.com>
2019-03-31 22:08:16 +0200

    Pi Push

--- VertretungsPlan.py
+++ VertretungsPlan.py
@@ -22,60 +22,60 @@ password.send_keys(u'\ue007') # Unicode 'Enter'
 
 browser.get('http://307.joomla.schule.bremen.de/index.php/service/sch%C3%BCler')
 
-try:
-    table_id = browser.find_element(By.CLASS_NAME, 'subst')
-    rowCount = table_id.find_elements_by_tag_name("tr")
-    intRow = 1
+while True:
 
-    for intRow in range(1, len(rowCount)): # iterates through every row (vertically) and skips the first one (header)
-        file.write("\n\t\t\t<tr>")
-        intCol = 0
-        rows = browser.find_elements_by_xpath('//table[2]/tbody/tr')[intRow]
-        
-        for intCol in range(0, 6): # iterates through every column in a row (horizontally) minus the student groups
-            cols = rows.find_elements_by_xpath('.//td')[intCol]
-            print(cols.text)
-            file.write("\n\t\t\t\t<th>" + cols.text + "<th>")
-            intCol += 1
+    try:
+        table_id = browser.find_element(By.CLASS_NAME, 'subst')
+        rowCount = table_id.find_elements_by_tag_name("tr")
+        intRow = 1
+
+        for intRow in range(1, len(rowCount)): # iterates through every row (vertically) and skips the first one (header)
+            file.write("\n\t\t\t<tr>")
+            intCol = 0
+            rows = browser.find_elements_by_xpath('//table[2]/tbody/tr')[intRow]
             
-        file.write("\n\t\t\t</tr>")
-        intRow += 1
-    file.write("\n\t\t</table>")
-except:
-    print("NullPointerException")
+            for intCol in range(0, 6): # iterates through every column in a row (horizontally) minus the student groups
+                cols = rows.find_elements_by_xpath('.//td')[intCol]
+                print(cols.text)
+                file.write("\n\t\t\t\t<th>" + cols.text + "<th>")
+                intCol += 1
+                
+            file.write("\n\t\t\t</tr>")
+            intRow += 1
+        file.write("\n\t\t</table>")
+    except:
+        print("NullPointerException")
 
 
-try:
-    # dayInfo = browser.find_elements_by_xpath('//div[2]/table[1]')
-    colsInfo = browser.find_elements_by_xpath('//*[@id="jsn-mainbody"]/div[2]/table[1]/tbody/tr[2]')
-    for info in colsInfo:
-        print(info.text)
-        file.write("\n\t\t<p>" + info.text + "</p>")
+    try:
+        # dayInfo = browser.find_elements_by_xpath('//div[2]/table[1]')
+        colsInfo = browser.find_elements_by_xpath('//*[@id="jsn-mainbody"]/div[2]/table[1]/tbody/tr[2]')
+        for info in colsInfo:
+            print(info.text)
+            file.write("\n\t\t<p>" + info.text + "</p>")
 
-except:
-    print("NullPointerException")
+    except:
+        print("NullPointerException")
 
 
-# try:
-#     substBlock = browser.find_element(By.CLASS_NAME, 'item-page')
-#     elements = substBlock.find_elements_by_tag_name("p")
-#     for elem in elements:
-#         print(elem.text)
-#         file.write("\n\t\t<p>" + elem.text + "</p>")
-# except:
-#     print("NullPointerException")
+    # try:
+    #     substBlock = browser.find_element(By.CLASS_NAME, 'item-page')
+    #     elements = substBlock.find_elements_by_tag_name("p")
+    #     for elem in elements:
+    #         print(elem.text)
+    #         file.write("\n\t\t<p>" + elem.text + "</p>")
+    # except:
+    #     print("NullPointerException")
 
-file.write("\n\t</body>\n</html>")
-browser.quit()
-file.close()
+    file.write("\n\t</body>\n</html>")
+    browser.quit()
+    file.close()
 
-def subprocess_cmd(command):
-    process = subprocess.Popen(command,stdout=subprocess.PIPE, shell=True)
-    proc_stdout = process.communicate()[0].strip()
-    print(proc_stdout)
+    def subprocess_cmd(command):
+        process = subprocess.Popen(command,stdout=subprocess.PIPE, shell=True)
+        proc_stdout = process.communicate()[0].strip()
+        print(proc_stdout)
 
-subprocess_cmd('cd /home/pi/djd4rkn355.github.io; git add --all; git commit -m "Pi Push"; git push')
-print("Success")
-
-# browser.quit()
-# file.close()
+    subprocess_cmd('cd /home/pi/djd4rkn355.github.io; git add --all; git commit -m "Pi Push"; git push')
+    print("Success")
+    time.sleep(480)
