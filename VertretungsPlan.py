29ff1c3c47cef3f1f548f29bd50a126ca1186031
djd4rkn355 <k.duezgoeren@gmail.com>
2019-03-31 22:31:58 +0200

    Pi Push

--- VertretungsPlan.py
+++ VertretungsPlan.py
@@ -5,24 +5,24 @@ import time
 import codecs
 import subprocess
 
-file = codecs.open("index.html", "w", "utf-8")
-file.truncate()
-file.write("<!DOCTYPE HTML>\n<html>\n\t<head>\n\t\t<meta charset=\"utf-8\">\n\t\t<style>\n\t\t\tbody {\n\t\t\t\tfont-family: Arial, Helvetica, sans-serif\n\t\t\t}\n\t\t</style>\n\t</head>\n\t<body>\n\t\t<table>")
-
-# browser = webdriver.Chrome('/Users/kduez/git/raspberryPi/chromedriver') # for Windows 10
-browser = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver') # for Raspberry Pi
-
-browser.get('http://307.joomla.schule.bremen.de/index.php/component/users/?view=login&Itemid=171')
-
-username = browser.find_element_by_id("username")
-password = browser.find_element_by_id("password")
-username.send_keys("deniz130")
-password.send_keys("Cinemassacre")
-password.send_keys(u'\ue007') # Unicode 'Enter'
-
-browser.get('http://307.joomla.schule.bremen.de/index.php/service/sch%C3%BCler')
-
 while True:
+
+    file = codecs.open("index.html", "w", "utf-8")
+    file.truncate()
+    file.write("<!DOCTYPE HTML>\n<html>\n\t<head>\n\t\t<meta charset=\"utf-8\">\n\t\t<style>\n\t\t\tbody {\n\t\t\t\tfont-family: Arial, Helvetica, sans-serif\n\t\t\t}\n\t\t</style>\n\t</head>\n\t<body>\n\t\t<table>")
+
+    # browser = webdriver.Chrome('/Users/kduez/git/raspberryPi/chromedriver') # for Windows 10
+    browser = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver') # for Raspberry Pi
+
+    browser.get('http://307.joomla.schule.bremen.de/index.php/component/users/?view=login&Itemid=171')
+
+    username = browser.find_element_by_id("username")
+    password = browser.find_element_by_id("password")
+    username.send_keys("deniz130")
+    password.send_keys("Cinemassacre")
+    password.send_keys(u'\ue007') # Unicode 'Enter'
+
+    browser.get('http://307.joomla.schule.bremen.de/index.php/service/sch%C3%BCler')
 
     try:
         table_id = browser.find_element(By.CLASS_NAME, 'subst')
