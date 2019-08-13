bed0dfeb7e3fa25d9ef7a908f63bb104da263c2d
djd4rkn355 <k.duezgoeren@gmail.com>
2019-08-13 13:13:29 +0200

    Changed path in FBBroadcast.py

--- FBBroadcast.py
+++ FBBroadcast.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import messaging
 from firebase_admin import credentials
 
-cred = credentials.Certificate("C:/Users/kduez/git/avh-plan-firebase-adminsdk-5iy97-2a377ca3d3.json")
+cred = credentials.Certificate("/home/pi/Desktop/avh-plan-firebase-adminsdk-5iy97-2a377ca3d3.json")
 firebase_admin.initialize_app(cred)
 
 topic = 'substitutions-broadcast'
