d53d8f0b7de242c033c5709e9a209ddc48369452
djd4rkn355 <k.duezgoeren@gmail.com>
2019-11-26 11:51:25 +0100

    Pi Push

--- Substitutions.py
+++ Substitutions.py
@@ -1,3 +1,4 @@
+# coding=utf_8
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from io import TextIOWrapper
---
+++ launcher.sh
@@ -0,0 +1,2 @@
+cd /home/pi/djd4rkn355.github.io
+sudo python Substitutions.py
